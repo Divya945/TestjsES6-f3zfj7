8304d4e9b58a7be0781700200bdf75821b802dff
Divya945 <divyadvs945@gmail.com>
2023-02-03 17:22:41 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Import stylesheets
 import './style.css';
 //import './newes6/spread.js';
-import './newes6/rest.js';
-import './newes6/string.js';
+//import './newes6/rest.js';
+//import './newes6/string.js';
+import './newes6/collections.js';
+
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1></h1>`;
